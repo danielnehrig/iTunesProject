f2e9451cdae2055e7bdb7f5edaa9845c87b11158
inet-pwnZ <inetpwnz@icloud.com>
2015-11-24 01:29:49 +0100

    travis update

---
+++ test/travis.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+result = `sass css/main.scss css/main.css`
+raise result unless $?.to_i == 0
+raise "When compiled the module should output some CSS" unless File.exists?('main.css')
+puts "Regular compile worked successfully"
